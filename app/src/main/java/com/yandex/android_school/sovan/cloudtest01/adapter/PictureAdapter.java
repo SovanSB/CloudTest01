001fbd7b72124b337e2abbf5b7b4554c2e20c6bc
SovanSB <SovanSB@gmail.com>
2015-10-25 17:05:26 +0300

    Picasso added, but not working well.

--- app/src/main/java/com/yandex/android_school/sovan/cloudtest01/adapter/PictureAdapter.java
+++ app/src/main/java/com/yandex/android_school/sovan/cloudtest01/adapter/PictureAdapter.java
@@ -2,11 +2,14 @@ package com.yandex.android_school.sovan.cloudtest01.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.yandex.android_school.sovan.cloudtest01.R;
 import com.yandex.android_school.sovan.cloudtest01.cloud.PictureItem;
 
@@ -26,7 +29,16 @@ public class PictureAdapter extends ResourceCursorAdapter {
         TextView fileName = (TextView) view.findViewById(R.id.textViewFileName);
         fileName.setText(name);
         final String url = cursor.getString(cursor.getColumnIndex(PictureItem.Columns.URL));
-        TextView fileUrl = (TextView) view.findViewById(R.id.textViewFileUrl);
-        fileUrl.setText(url);
+       // TextView fileUrl = (TextView) view.findViewById(R.id.textViewFileUrl);
+       // fileUrl.setText(url);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        if (TextUtils.isEmpty(url))
+        {
+            imageView.setVisibility(View.GONE);
+        }
+        else {
+            imageView.setVisibility(View.VISIBLE);
+            Picasso.with(context).load(url).into(imageView);
+        }
     }
 }
